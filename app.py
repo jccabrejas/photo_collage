417de2d532ead04e0d509acd85c007a8ba9cb932
JC Cabrejas <j.c.cabrejas@gmail.com>
2024-12-14 17:34:51 +0100

    draggable photo thumbs

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import flet as ft
 
-from photos import handle_file_picker
-
 
 def main(page: ft.Page):
     page.title = "Photo collage"
-    page.window.width=1000
-    page.window.height=700
-    page.padding=0
+    page.window.width = 800
+    page.window.height = 700
+    page.padding = 0
     page.bgcolor = ft.Colors.GREY_600
     page.theme_mode = ft.ThemeMode.DARK
     page.theme = ft.Theme(
@@ -18,7 +16,7 @@ def main(page: ft.Page):
             secondary=ft.Colors.ORANGE,
             background=ft.Colors.GREY_900,
             surface=ft.Colors.GREY_800,
-        )
+        ),
     )
     page.vertical_alignment = ft.MainAxisAlignment.START
 
@@ -36,33 +34,83 @@ def main(page: ft.Page):
             work_area.content = ft.Text("Background", size=24)
         work_area.update()
 
+    def handle_file_picker(e: ft.FilePickerResultEvent):
+
+        for i in e.files:
+            photos_area.controls.append(
+                ft.Draggable(
+                    group="photo",
+                    content=ft.Image(
+                        src=i.path,
+                        width=100,
+                        height=100,
+                        fit=ft.ImageFit.SCALE_DOWN,
+                        repeat=ft.ImageRepeat.NO_REPEAT,
+                        border_radius=ft.border_radius.all(10),
+                    ),
+                )
+            )
+            photos_area.controls.append(ft.Text(value=i.name))
+            photos_area.update()
+
     file_picker = ft.FilePicker(on_result=handle_file_picker)
     page.overlay.append(file_picker)
 
-    selected_files_text = ft.Text(
-        "No files selected",
-        size=14,
-        color=ft.Colors.WHITE
+    photos_area = ft.Column(
+        controls=[
+            ft.FilledButton(
+                text="Select photos",
+                icon=ft.Icons.FOLDER_OPEN,
+                color=ft.Colors.WHITE,
+                bgcolor=ft.Colors.BLUE,
+                on_click=lambda _: file_picker.pick_files(allow_multiple=True),
+            ),
+        ],
+        width=300,
+        expand=False,
+        alignment=ft.MainAxisAlignment.START,
+        scroll="always",
     )
 
-    photos_area = ft.Column(
-                controls=[
-                    ft.FilledButton(
-                        text="Select photos",
-                        icon=ft.Icons.FOLDER_OPEN,
-                        color=ft.Colors.WHITE,
-                        bgcolor=ft.Colors.BLUE,
-                        on_click=lambda _: file_picker.pick_files(allow_multiple=True)
-                    ),
-                    selected_files_text,
-                ],
-                width=300,
-                expand=False,
-                alignment=ft.MainAxisAlignment.START,
-            )
+    def drag_will_accept(e):
+        e.control.content.border = ft.border.all(
+            2, ft.Colors.BLACK45 if e.data == "true" else ft.Colors.RED
+        )
+        # e.control.content.update()
+        e.control.update()
+
+    def drag_accept(e):
+        src = e.control.page.get_control(e.src_id)
+        e.control.content.content = src.content
+        e.control.content.content.src = src.content.src
+        e.control.content.content.fit = ft.ImageFit.SCALE_DOWN
+        e.control.content.border = None
+        e.control.content.content.update()
+        e.control.content.update()
+        e.control.update()
+        work_area.update()
+        content_area.update()
+
+    def drag_leave(e):
+        e.control.content.border = None
+        e.control.update()
 
     content_area = ft.Container(
-        content=ft.Text("placeholder text", size=24),
+        content=ft.Column(
+            [
+                ft.DragTarget(
+                    group="photo",
+                    content=ft.Image(
+                        src=r"C:\OneDrive\Dokumente\repos\photo_collage\placeholder.webp",
+                        width=150,
+                        height=150,
+                        ),
+                    on_will_accept=drag_will_accept,
+                    on_accept=drag_accept,
+                    on_leave=drag_leave,
+                ),
+            ]
+            ),
         expand=True,
         padding=30,
         width=300,
@@ -91,27 +139,27 @@ def main(page: ft.Page):
             ft.NavigationRailDestination(
                 icon=ft.Icons.PHOTO_LIBRARY,
                 selected_icon=ft.Icons.PHOTO_LIBRARY_OUTLINED,
-                label='Photos'
+                label="Photos",
             ),
             ft.NavigationRailDestination(
                 icon=ft.Icons.PAGES,
                 selected_icon=ft.Icons.PAGES_OUTLINED,
-                label="Layouts"
+                label="Layouts",
             ),
             ft.NavigationRailDestination(
                 icon=ft.Icons.BUILD,
                 selected_icon=ft.Icons.BUILD_OUTLINED,
-                label="New Layout"
+                label="New Layout",
             ),
             ft.NavigationRailDestination(
                 icon=ft.Icons.SAVE,
                 selected_icon=ft.Icons.SAVE_OUTLINED,
-                label="Save Collage"
+                label="Save Collage",
             ),
             ft.NavigationRailDestination(
                 icon=ft.Icons.COLOR_LENS,
                 selected_icon=ft.Icons.COLOR_LENS_OUTLINED,
-                label="Background"
+                label="Background",
             ),
         ],
         on_change=change_view,
